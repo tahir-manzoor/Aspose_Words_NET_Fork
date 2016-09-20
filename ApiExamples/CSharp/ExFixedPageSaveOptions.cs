66a49f11403ceb7d9158701403b3478d436c2d05
vderyushev <falleretic@gmail.com>
2016-09-21 03:05:24 +1200

    Add FixedPageSaveOptions tests

--- ApiExamples/CSharp/ExFixedPageSaveOptions.cs
+++ ApiExamples/CSharp/ExFixedPageSaveOptions.cs
@@ -13,49 +13,84 @@ using Aspose.Words.Saving;
 using NUnit.Framework;
 
 using System.Collections.Generic;
+using System.IO;
+using Aspose.Words.Rendering;
 
 namespace ApiExamples
 {
     [TestFixture]
     internal class ExFixedPageSaveOptions : ApiExampleBase
     {
-        public IEnumerable<TestCaseData> CountEqualsZeroAndHouseGrossIsGreaterTestCases
+        public static IEnumerable<TestCaseData> JpegQualityData
         {
             get
             {
+                yield return new TestCaseData(new HtmlFixedSaveOptions());
+                yield return new TestCaseData(new ImageSaveOptions(SaveFormat.Bmp));
+                yield return new TestCaseData(new PdfSaveOptions());
+                yield return new TestCaseData(new PsSaveOptions());
+                yield return new TestCaseData(new SvgSaveOptions());
                 yield return new TestCaseData(new XamlFixedSaveOptions());
                 yield return new TestCaseData(new XpsSaveOptions());
                 yield return new TestCaseData(new SwfSaveOptions());
             }
         }
 
-        public IEnumerable<TestCaseData> MetafileRenderingOptions
+        public static IEnumerable<TestCaseData> MetafileRenderingData
         {
             get
             {
                 yield return new TestCaseData(new XamlFixedSaveOptions(), MetafileRenderingMode.Bitmap);
-                yield return new TestCaseData(new XamlFixedSaveOptions(), MetafileRenderingMode.Vector);
-                yield return new TestCaseData(new XamlFixedSaveOptions(), MetafileRenderingMode.VectorWithFallback);
-                yield return new TestCaseData(new XpsSaveOptions(), MetafileRenderingMode.Bitmap);
                 yield return new TestCaseData(new XpsSaveOptions(), MetafileRenderingMode.Vector);
-                yield return new TestCaseData(new XpsSaveOptions(), MetafileRenderingMode.VectorWithFallback);
-                yield return new TestCaseData(new SwfSaveOptions(),MetafileRenderingMode.Bitmap);
-                yield return new TestCaseData(new SwfSaveOptions(), MetafileRenderingMode.Vector);
                 yield return new TestCaseData(new SwfSaveOptions(), MetafileRenderingMode.VectorWithFallback);
             }
         }
 
-        //ToDo: Need to add asserts
-        [Test]
-        [TestCaseSource(nameof(CountEqualsZeroAndHouseGrossIsGreaterTestCases))]
-        public void JpegQualityDefaultValue(FixedPageSaveOptions objectSaveOptions)
+        public static IEnumerable<TestCaseData> NumeralFormatData
         {
-            FixedPageSaveOptions saveOptions = objectSaveOptions;
-            Assert.AreEqual(95, saveOptions.JpegQuality);
+            get
+            {
+                yield return new TestCaseData(new HtmlFixedSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new ImageSaveOptions(SaveFormat.Docx), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new PdfSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new PsSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new SvgSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new XamlFixedSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new XpsSaveOptions(), NumeralFormat.EasternArabicIndic);
+                yield return new TestCaseData(new SwfSaveOptions(), NumeralFormat.European);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> PageCountData
+        {
+            get
+            {
+                yield return new TestCaseData(new HtmlFixedSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new ImageSaveOptions(SaveFormat.Docx), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new PdfSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new PsSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new SvgSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new XamlFixedSaveOptions(), NumeralFormat.ArabicIndic);
+                yield return new TestCaseData(new XpsSaveOptions(), NumeralFormat.EasternArabicIndic);
+                yield return new TestCaseData(new SwfSaveOptions(), NumeralFormat.European);
+            }
         }
 
         [Test]
-        [TestCaseSource(nameof(MetafileRenderingOptions))]
+        [TestCaseSource(nameof(JpegQualityData))]
+        public void JpegQualityDefaultValue(FixedPageSaveOptions objectSaveOptions)
+        {
+            Document doc = new Document();
+
+            FixedPageSaveOptions saveOptions = objectSaveOptions;
+            Assert.AreEqual(95, saveOptions.JpegQuality);
+
+            MemoryStream dstStream = new MemoryStream();
+            doc.Save(dstStream, saveOptions);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(MetafileRenderingData))]
         public void MetafileRendering(FixedPageSaveOptions objectSaveOptions, MetafileRenderingMode metafileRendering)
         {
             Document doc = new Document();
@@ -63,7 +98,34 @@ namespace ApiExamples
             FixedPageSaveOptions saveOptions = objectSaveOptions;
             saveOptions.MetafileRenderingOptions.RenderingMode = metafileRendering;
 
-            doc.Save(MyDir + @"\Artifacts\MetafileRendering.swf", saveOptions);
+            MemoryStream dstStream = new MemoryStream();
+            doc.Save(dstStream, saveOptions);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(NumeralFormatData))]
+        public void RenderingOfNumerals(FixedPageSaveOptions objectSaveOptions, NumeralFormat numeralFormat)
+        {
+            Document doc = new Document();
+
+            FixedPageSaveOptions saveOptions = objectSaveOptions;
+            saveOptions.NumeralFormat = numeralFormat;
+
+            MemoryStream dstStream = new MemoryStream();
+            doc.Save(dstStream, saveOptions);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(PageCountData))]
+        public void Num(FixedPageSaveOptions objectSaveOptions, int pageCount)
+        {
+            Document doc = new Document();
+
+            FixedPageSaveOptions saveOptions = objectSaveOptions;
+            saveOptions.PageCount = pageCount;
+
+            MemoryStream dstStream = new MemoryStream();
+            doc.Save(dstStream, saveOptions);
         }
     }
 }
