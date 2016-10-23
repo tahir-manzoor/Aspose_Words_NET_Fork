9527db7582b393ff78c02c744548c5cdca9a9b0d
vderyushev <falleretic@gmail.com>
2016-10-24 04:48:12 +1300

    optimized method

---
+++ ApiExamples/CSharp/TestData/TestTables.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ApiExamples.TestData
+{
+    internal static class TestTables
+    {
+        internal static DataSet AddClientsTestData()
+        {
+            Random rnd = new Random();
+            DataSet ds = new DataSet();
+
+            int j = 1;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                ds.Clients.Rows.Add(i, "Name " + i);
+            }
+
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int y = j; y <= j + 2; y++)
+                {
+                    ds.Contracts.Rows.Add(y, y, i, rnd.Next(2000, 100000));
+                }
+
+                j = j + 3;
+            }
+
+            for (int i = 1; i <= 3; i++)
+            {
+                ds.Managers.Rows.Add(i, "Name " + i);
+            }
+
+            return ds;
+        }
+    }
+}
